cb1e62d1fcbcba559534d5632f229e3129f3a49f
Thundercat <michael.c.haines@gmail.com>
2015-07-05 09:49:35 -0600

    initial commit

---
+++ Wasatch-Bike-Support-Meteor.js
@@ -0,0 +1,44 @@
+if (Meteor.isClient) {
+  Template.navbar.helpers({
+    activeIfTemplateIs: function (template) {
+      var currentRoute = Router.current();
+      return currentRoute &&
+        template === currentRoute.lookupTemplate() ? 'active' : '';
+    }
+  });
+}
+
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+Router.configure({
+  layoutTemplate: 'layout',
+  notFoundTemplate: 'index'
+});
+
+
+
+Router.route('/', function () {
+  this.render('index');
+});
+
+
+Router.route('/about', function () {
+  this.render('about');
+});
+
+Router.route('/services', function(){
+	this.render('services');
+})
+
+Router.route('/contact', function(){
+	this.render('contact');
+})
+
+Router.route('/servicesNew', function(){
+  this.render('servicesNew');
+})
